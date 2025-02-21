86c5e900adc41656ba82c4379184f56bda924fd4
shriramch <shriramchandran253@gmail.com>
2025-02-21 12:18:48 +0000

    Deploying to gh-pages from @ shriramch/shriramch.github.io@71739581d2cc72085639c24bef6a17495b179088 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,14 +10,14 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-cv",
-          title: "cv",
+          title: "CV",
           description: "This is my Curriculum Vitae.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
           },
         },{id: "nav-projects",
-          title: "projects",
+          title: "Projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
@@ -399,51 +399,31 @@ ninja.data = [{
           section: "News",},{id: "news-our-work-on-maximus-during-my-semester-project-submitted-on-11-october-2024-was-accepted-at-sigmod-2025",
           title: 'Our work on Maximus during my semester project, submitted on 11 October 2024,...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-master-thesis-ongoing",
+          title: 'Master Thesis (ongoing)',
+          description: "with the Scalable Parallel Computing Laboratory",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-integrating-the-gpu-backend-into-the-maximus-db-engine",
+          title: 'Integrating the GPU Backend Into the Maximus DB Engine',
+          description: "Practical Work - Semester Project with the Systems Group",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-starplat-graph-dsl-program-analysis",
+          title: 'StarPlat Graph DSL - Program Analysis',
+          description: "Bachelor Project with Prof. Rupesh Nasre",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-a-study-of-stability-of-discrete-graph-optimization-problems",
+          title: 'A Study of Stability of Discrete Graph Optimization Problems',
+          description: "Undergraduate Research with Prof. Raghavendra Rao",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-software-engineering-internships",
+          title: 'Software Engineering Internships',
+          description: "My internships at Rubrik and Godot Media",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
