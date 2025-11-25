b345b13bd1aab2c06fcca538cfbe91cfc34f6c87
shriramch <shriramchandran253@gmail.com>
2025-11-25 14:04:51 +0000

    Deploying to gh-pages from @ shriramch/shriramch.github.io@037d76d61210bbb30d6793dbaeae15a4bae7f2f1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -27,17 +27,6 @@ ninja.data = [{
           window.location.href = "/blog/2024/photo-gallery/";
         
       },
-    },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-      
-        title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-        
-      },
     },{id: "post-a-post-with-tabs",
       
         title: "a post with tabs",
@@ -258,17 +247,6 @@ ninja.data = [{
           window.location.href = "/blog/2022/giscus-comments/";
         
       },
-    },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-      
-        title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-        
-      },
     },{id: "post-a-post-with-redirect",
       
         title: "a post with redirect",
@@ -394,6 +372,9 @@ ninja.data = [{
           description: "",
           section: "News",},{id: "news-i-have-completed-my-master-s-thesis-with-scalable-parallel-computing-laboratory-at-eth-we-have-published-our-work-on-arxiv",
           title: 'I have completed my master’s thesis with Scalable Parallel Computing Laboratory at ETH....',
+          description: "",
+          section: "News",},{id: "news-excited-to-join-spcl-as-a-phd-student-looking-forward-to-learning-and-making-great-connections",
+          title: 'Excited to join SPCL as a PhD student. Looking forward to learning and...',
           description: "",
           section: "News",},{id: "projects-master-thesis-ongoing",
           title: 'Master Thesis (ongoing)',
